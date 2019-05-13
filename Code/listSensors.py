c8a321d212eab809c2853f005c41216e552b8894
LeDon <delangedon@gmail.com>
2019-05-13 15:29:10 +0200

    removed function
    was not being used anymore

--- Code/listSensors.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-
-def listSensors(df, path):
-    df = df.drop_duplicates(subset="Sensor").reset_index()
-    df = df[["Sensor", "SensorLatitude", "SensorLongitude"]]
-
-    df.to_csv(path, index=False)
