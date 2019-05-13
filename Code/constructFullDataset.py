c6d509bfb0cdbc5d59a8f4191cce01c299c11e97
LeDon <delangedon@gmail.com>
2019-05-13 16:46:15 +0200

    Implemented needed parameters

--- Code/constructFullDataset.py
+++ Code/constructFullDataset.py
@@ -5,31 +5,35 @@ import re
 import numpy as np
 
 #Import Functions other files
-import ImportData.importFiles as im
-import ImportData.exportFiles as ex
 import ImportData.CombineData as cmd 
 import ImportData.EventData as evd 
 import ImportData.GVBData as gvb 
-import ImportData.CrowdednessData as cwd 
+import ImportData.SensorData as sd 
 
-# Crowdedness Variables
+# Sensor Variables
 #Path to original crowdedness dataset
-path_to_crowdednessData = '../../../Data_thesis/CMSA/cmsa_data.xlsx'
+path_to_sensorData = '../../../Data_thesis/CMSA/cmsa_data.xlsx'
 
 #Path to Sensor Data
-path_to_sensorData = '../../../Data_thesis/Open_Data/crowdedness_sensoren.csv'
+path_to_coordinateData = '../../../Data_thesis/Open_Data/crowdedness_sensoren.csv'
 
 #Path to Blip Data
 path_to_blipData = "../../../Data_thesis/CMSA/BlipData.csv"
 
+#Where to save Longitude Scaler
+lon_scaler_filename = "../../../Data_thesis/Models/lon_scaler.sav"
+
+#Where to save Latitude Scaler
+lat_scaler_filename = "../../../Data_thesis/Models/lat_scaler.sav"
+
 #Sensors to use in Sensor Data
-needed_sensors = ["GAWW-01", "GAWW-02", "GAWW-03", "GAWW-04", "GAWW-05", "GAWW-06", "GAWW-07", "GAWW-08", "GAWW-09",
-                  "GAWW-10"]
+needed_sensors = ["GAWW-01", "GAWW-02", "GAWW-03","GAWW-04", "GAWW-05", "GAWW-06", "GAWW-07"]
 
 #Alternative names Sensors
 gaww_02 = [2, "02R", "2R", "Oude Kennissteeg Occ wifi"]
 gaww_03 = [3, "03R"]
 
+#################################################################################
 
 # GVB Variables
 #Path to arrival data
@@ -39,8 +43,9 @@ path_to_arr_data = "../../../Data_thesis/GVB/Datalab_Reis_Bestemming_Uur_2019040
 path_to_dep_data = "../../../Data_thesis/GVB/Datalab_Reis_Herkomst_Uur_20190403.csv"
 
 #Stations to be used
-stations = ["Nieuwmarkt", "Nieuwezijds Kolk", "Dam", "Spui"]
+stations = ["Nieuwmarkt", "Nieuwezijds Kolk", "Dam", "Spui", "Centraal Station"]
 
+#################################################################################
 
 #Event Variables
 #path to Event database in JSON format
@@ -61,21 +66,30 @@ start_date = pd.Timestamp(2018, 3, 11)
 #End date for relevant events
 end_date = pd.Timestamp(2019, 4, 30)
 
+#################################################################################
+
+#Combine Variables
+
+#Where to save the station weight scaler
+station_scaler_filename = "../../../Data_thesis/Models/station_scaler.sav"
+
+#################################################################################
+
 #Where to save df
 full_df_path = "../../../Data_thesis/Full_Datasets/Full.csv"
 
 
 def main():
 
-    crowd_df = cwd.CrowdednessDF(path_to_crowdednessData, path_to_sensorData,
-                                 path_to_blipData, needed_sensors, gaww_02, gaww_03)
+    sensor_df = sd.sensorDF(path_to_sensorData, path_to_coordinateData, path_to_blipData,
+                            needed_sensors, gaww_02, gaww_03, lon_scaler_filename, lat_scaler_filename)
 
-    gvb_df = gvb.GVBDF(path_to_arr_data, path_to_dep_data, stations)
+    gvb_df = gvb.gvbDF(path_to_arr_data, path_to_dep_data, stations)
 
-    event_df = evd.EventDF(json_events_path, lon_low,
+    event_df = evd.eventDF(json_events_path, lon_low,
                            lon_high, lat_low, lat_high, start_date, end_date)
     
-    full_df = cmd.CombineDF(crowd_df, gvb_df, event_df)
+    full_df = cmd.fullDF(sensor_df, gvb_df, event_df, stations, station_scaler_filename)
 
     #Converts DF to CSV
     full_df.to_csv(full_df_path, index=False)
