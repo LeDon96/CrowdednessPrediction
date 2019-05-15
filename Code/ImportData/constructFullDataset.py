0fcf61c27b48f9d2b516fff84c1ee675096f8c9b
LeDon <delangedon@gmail.com>
2019-05-15 21:28:12 +0200

    construct function full df

--- Code/ImportData/constructFullDataset.py
+++ Code/ImportData/constructFullDataset.py
@@ -5,97 +5,25 @@ import re
 import numpy as np
 
 #Import Functions other files
-import ImportData.CombineData as cmd 
-import ImportData.EventData as evd 
-import ImportData.GVBData as gvb 
-import ImportData.SensorData as sd 
+import Code.ImportData.CombineData as cmd 
+import Code.ImportData.EventData as evd 
+import Code.ImportData.GVBData as gvb 
+import Code.ImportData.SensorData as sd 
 
-# Sensor Variables
-#Path to original crowdedness dataset
-path_to_sensorData = '../../../Data_thesis/CMSA/cmsa_data.xlsx'
+def constructDF(input_dict, output_dict, params_dict):
 
-#Path to Sensor Data
-path_to_coordinateData = '../../../Data_thesis/Open_Data/crowdedness_sensoren.csv'
+    sensor_df = sd.sensorDF(input_dict["sensorData"], input_dict["coordinateData"], input_dict["blipData"],
+                            params_dict["needed_sensors"], params_dict["gaww_02"], params_dict["gaww_03"],
+                            output_dict["lon_scaler"], output_dict["lat_scaler"])
 
-#Path to Blip Data
-path_to_blipData = "../../../Data_thesis/CMSA/BlipData.csv"
+    gvb_df, average_df = gvb.gvbDF(input_dict["arrData"], input_dict["deppData"], params_dict["stations"])
 
-#Where to save Longitude Scaler
-lon_scaler_filename = "../../../Data_thesis/Models/lon_scaler.sav"
-
-#Where to save Latitude Scaler
-lat_scaler_filename = "../../../Data_thesis/Models/lat_scaler.sav"
-
-#Sensors to use in Sensor Data
-needed_sensors = ["GAWW-01", "GAWW-02", "GAWW-03","GAWW-04", "GAWW-05", "GAWW-06", "GAWW-07"]
-
-#Alternative names Sensors
-gaww_02 = [2, "02R", "2R", "Oude Kennissteeg Occ wifi"]
-gaww_03 = [3, "03R"]
-
-#################################################################################
-
-# GVB Variables
-#Path to arrival data
-path_to_arr_data = "../../../Data_thesis/GVB/Datalab_Reis_Bestemming_Uur_20190402.csv"
-
-#path to departure data
-path_to_dep_data = "../../../Data_thesis/GVB/Datalab_Reis_Herkomst_Uur_20190403.csv"
-
-#Stations to be used
-stations = ["Nieuwmarkt", "Nieuwezijds Kolk", "Dam", "Spui", "Centraal Station"]
-
-#################################################################################
-
-#Event Variables
-#path to Event database in JSON format
-json_events_path = "../../../Data_thesis/Open_Data/Evenementen.json"
-
-#Parameters for area to search in
-#longitude
-lon_low = 4.88
-lon_high = 4.92
-
-#Latitude
-lat_low = 52.36
-lat_high = 52.39
-
-#Start date for relevant events
-start_date = pd.Timestamp(2018, 3, 11)
-
-#End date for relevant events
-end_date = pd.Timestamp(2019, 4, 30)
-
-#################################################################################
-
-#Combine Variables
-
-#Where to save the station weight scaler
-station_scaler_filename = "../../../Data_thesis/Models/station_scaler.sav"
-
-#################################################################################
-
-#Where to save df
-full_df_path = "../../../Data_thesis/Full_Datasets/Full.csv"
-average_df_path = "../../../Data_thesis/Full_Datasets/AveragePassengerCounts.csv"
-
-
-def main():
-
-    sensor_df = sd.sensorDF(path_to_sensorData, path_to_coordinateData, path_to_blipData,
-                            needed_sensors, gaww_02, gaww_03, lon_scaler_filename, lat_scaler_filename)
-
-    gvb_df, average_df = gvb.gvbDF(
-        path_to_arr_data, path_to_dep_data, stations)
-
-    event_df = evd.eventDF(json_events_path, lon_low,
-                           lon_high, lat_low, lat_high, start_date, end_date)
+    event_df = evd.eventDF(input_dict["eventData"], params_dict["lon_low"], params_dict["lon_high"], 
+                           params_dict["lat_low"], params_dict["lat_high"])
     
-    full_df = cmd.fullDF(sensor_df, gvb_df, event_df, stations, station_scaler_filename)
+    full_df = cmd.fullDF(sensor_df, gvb_df, event_df,
+                         params_dict["stations"], output_dict["station_scaler"])
 
     #Converts DF to CSV
-    full_df.to_csv(full_df_path, index=False)
-    average_df.to_csv(average_df_path, index=True)
-
-if __name__ == '__main__':
-	main()
+    full_df.to_csv(output_dict["full_df"], index=False)
+    average_df.to_csv(output_dict["average_passenger_counts"], index=True)
